f039da106d4153e7e65fe26678768b5b16f1cb3a
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-28 16:05:46 -0300

    feat: add CVBuilder to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import "./App.css";
-import { CVPreview } from "@/components";
-import type { PersonalInfo } from "./models";
+import { CVBuilder } from "@/components";
 
 function App() {
-  const mockPersonalInfo: PersonalInfo = {
-    name: "Juan",
-    jobTitle: "Software Engineer",
-    email: "juan@mail.com",
-    phone: 2241565656,
-    city: "New York",
-  };
-
   return (
     <>
       <main>
-        <CVPreview personalInfo={mockPersonalInfo} />
+        <CVBuilder />
       </main>
     </>
   );
