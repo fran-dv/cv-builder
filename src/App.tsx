88197815428fe03643db58644e74533af778d13d
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-05-28 10:44:15 -0300

    feat: use CVPreview in App with mock info and add App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import "./App.css";
+import { CVPreview } from "@/components";
+import type { PersonalInfo } from "./models";
+
 function App() {
-  return <></>;
+  const mockPersonalInfo: PersonalInfo = {
+    name: "Juan",
+    jobTitle: "Software Engineer",
+    email: "juan@mail.com",
+    phone: 2241565656,
+    city: "New York",
+  };
+
+  return (
+    <>
+      <main>
+        <CVPreview personalInfo={mockPersonalInfo} />
+      </main>
+    </>
+  );
 }
 
 export default App;
