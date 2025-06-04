a9c89ea664ac7d489216d15fc311972fdece6748
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-04 09:21:59 -0300

    feat: add Experiences section to the CVPreview component

--- src/components/CVPreview/CVPreview.tsx
+++ src/components/CVPreview/CVPreview.tsx
@@ -1,10 +1,13 @@
-import type { PersonalInfo, SummaryInfo } from "@/models";
+import type { ExperienceInfo, PersonalInfo, SummaryInfo } from "@/models";
 import "./CVPreview.css";
 import { Icon } from "@/components";
 
 interface Props {
   personalInfo: PersonalInfo;
   summaryInfo: SummaryInfo;
+  experienceInfo1: ExperienceInfo;
+  experienceInfo2: ExperienceInfo;
+  experienceInfo3: ExperienceInfo;
 }
 
 interface CVSectionProps {
@@ -25,7 +28,24 @@ const CVSection = ({ title, children }: CVSectionProps) => {
 export const CVPreview = ({
   personalInfo: { name, jobTitle, email, phone, city },
   summaryInfo: { summary },
+  experienceInfo1,
+  experienceInfo2,
+  experienceInfo3,
 }: Props) => {
+  const experiences = [
+    experienceInfo1,
+    experienceInfo2,
+    experienceInfo3,
+  ].filter(
+    (exp) =>
+      exp &&
+      (exp.jobTitle ||
+        exp.employer ||
+        exp.startDate ||
+        exp.finishDate ||
+        exp.city ||
+        exp.description),
+  );
   return (
     <div className="cv-preview">
       {name && <h2 className="name">{name}</h2>}
@@ -53,6 +73,41 @@ export const CVPreview = ({
       {summary && (
         <CVSection title="Summary">
           <p>{summary}</p>
+        </CVSection>
+      )}
+
+      {experiences.length > 0 && (
+        <CVSection title="Experiences">
+          {[experienceInfo1, experienceInfo2, experienceInfo3].map(
+            (experience, index) => {
+              if (!experience) return null;
+
+              return (
+                <div key={`experience${index}`} className="section-item">
+                  <div className="item-heading">
+                    <div className="item-title">
+                      <h4>{experience.jobTitle}</h4>
+                      <p>
+                        {experience.employer ? `at ${experience.employer}` : ""}
+                      </p>
+                    </div>
+
+                    <div className="item-place-dates">
+                      <p>
+                        {experience.startDate
+                          ? `${experience.startDate} - `
+                          : ""}{" "}
+                        {experience.finishDate}
+                      </p>
+                      <p className="item-place">{experience.city}</p>
+                    </div>
+                  </div>
+
+                  <p>{experience.description}</p>
+                </div>
+              );
+            },
+          )}
         </CVSection>
       )}
     </div>
