6a8d1b8e4a9612011e01af587d5835787b3cc2f7
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-04 15:05:59 -0300

    fix: import icons assets correctly

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,3 +1,15 @@
+import emailIcon from "/src/assets/email-icon.svg";
+import phoneIcon from "@/assets/phone-icon.svg";
+import cityIcon from "@/assets/city-icon.svg";
+import exitIcon from "@/assets/exit-icon.svg";
+import previewIcon from "@/assets/preview-icon.svg";
+import arrowDownIcon from "@/assets/arrow-down-icon.svg";
+import personIcon from "@/assets/person-icon.svg";
+import summaryIcon from "@/assets/summary-icon.svg";
+import deleteIcon from "@/assets/delete-icon.svg";
+import experienceIcon from "@/assets/experience-icon.svg";
+import educationIcon from "@/assets/education-icon.svg";
+
 interface IconsFiles {
   email: string;
   phone: string;
@@ -13,17 +25,17 @@ interface IconsFiles {
 }
 
 const IconsFileNames: IconsFiles = {
-  email: "email-icon.svg",
-  phone: "phone-icon.svg",
-  city: "city-icon.svg",
-  exit: "exit-icon.svg",
-  preview: "preview-icon.svg",
-  arrowDown: "arrow-down-icon.svg",
-  person: "person-icon.svg",
-  summary: "summary-icon.svg",
-  delete: "delete-icon.svg",
-  experience: "experience-icon.svg",
-  education: "education-icon.svg",
+  email: emailIcon,
+  phone: phoneIcon,
+  city: cityIcon,
+  exit: exitIcon,
+  preview: previewIcon,
+  arrowDown: arrowDownIcon,
+  person: personIcon,
+  summary: summaryIcon,
+  delete: deleteIcon,
+  experience: experienceIcon,
+  education: educationIcon,
 } as const;
 
 export type IconsTypes = keyof IconsFiles;
@@ -37,5 +49,5 @@ interface Props {
 export const Icon = ({ type, className = "icon" }: Props) => {
   const fileName = IconsFileNames[type];
   const alt = `${type[0].toUpperCase() + type.slice(1)} icon`;
-  return <img className={className} src={`src/assets/${fileName}`} alt={alt} />;
+  return <img className={className} src={fileName} alt={alt} />;
 };
