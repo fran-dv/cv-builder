ac768115ad423d509cba77ab619b0f1c6657ca50
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-04 09:14:35 -0300

    refactor: add support for multiple items in the FormAccordion component

--- src/components/FormAccordion/FormAccordion.tsx
+++ src/components/FormAccordion/FormAccordion.tsx
@@ -1,20 +1,27 @@
 import type { callbackProps, FormField } from "@/components";
-import React from "react";
+import React, { useState } from "react";
 import { IconButton, Form } from "@/components";
 import "./FormAccordion.css";
-import type { CVData } from "@/models";
+import { isExperienceInfo, type CVData } from "@/models";
+
+interface MultiDataTypes<K extends keyof CVData> {
+  "1": K;
+  "2": K;
+  "3": K;
+}
 
 interface Props<K extends keyof CVData> {
   fields: FormField[];
-  dataType: K;
+  dataType: K | MultiDataTypes<K>; // if multi item, specify the type of each item
   onChange: ({ data, dataType }: callbackProps) => void;
-  onSubmit: ({ data, dataType }: callbackProps) => void;
+  onSubmit: (close?: boolean) => void;
   onExitWithoutSubmit: () => void;
   title: string;
   isActive: boolean;
   onShow: () => void;
   blockIcon?: React.ReactNode;
   currentData: CVData;
+  multiItems?: boolean;
 }
 
 export const FormAccordion = <K extends keyof CVData>({
@@ -28,14 +35,69 @@ export const FormAccordion = <K extends keyof CVData>({
   onShow,
   blockIcon,
   currentData,
+  multiItems,
 }: Props<K>) => {
+  type ItemWithKey<K extends keyof CVData> = {
+    key: K;
+    value: CVData[K];
+    id: string;
+  };
+  const [editingItem, setEditingItem] = useState<boolean>(false);
+  const [existingItems, setExistingItems] = useState<Array<ItemWithKey<K>>>([]);
+
   const handleClose = () => {
     onExitWithoutSubmit();
+    setEditingItem(false);
   };
 
   const showForm = () => {
     onShow();
   };
+
+  const findFirstFreeItemSlot = <K extends string>(
+    items: Array<{ key: K }>,
+  ) => {
+    const usedSlots = new Set(
+      items.map((it) => {
+        const lastChar = it.key.charAt(it.key.length - 1);
+        return lastChar as "1" | "2" | "3";
+      }),
+    );
+
+    if (!usedSlots.has("1")) return "1";
+    if (!usedSlots.has("2")) return "2";
+    if (!usedSlots.has("3")) return "3";
+    return "-1";
+  };
+
+  const handleFormSubmit = ({ data, dataType }: callbackProps) => {
+    if (multiItems) {
+      onChange({ data, dataType });
+    }
+    const close = multiItems ? false : true;
+    onSubmit(close); // don't close the dropdown if multi items
+
+    if (multiItems && data) {
+      const theKey = dataType as K;
+      const itemWithKey: ItemWithKey<K> = {
+        key: theKey,
+        value: data,
+        id: crypto.randomUUID(),
+      };
+      setExistingItems([...existingItems, itemWithKey]);
+      setEditingItem(false);
+    }
+  };
+
+  const deleteItem = (key: K, id: string) => {
+    setExistingItems(existingItems.filter((item) => item.id !== id));
+
+    // update currentData from parent
+    onChange({ data: {}, dataType: key });
+    onSubmit(false);
+  };
+
+  const nextSlot = findFirstFreeItemSlot(existingItems);
 
   return (
     <div className={`info-block ${isActive ? "expanded" : ""}`}>
@@ -49,17 +111,63 @@ export const FormAccordion = <K extends keyof CVData>({
         />
       </div>
 
-      {isActive && (
+      {(!multiItems || editingItem) && isActive && (
         <div>
           <Form
             fields={fields}
             onChange={onChange}
-            onSubmit={onSubmit}
-            dataType={dataType}
+            onSubmit={handleFormSubmit}
+            dataType={
+              multiItems && nextSlot !== "-1"
+                ? (dataType as MultiDataTypes<K>)[nextSlot]
+                : (dataType as K)
+            }
             currentData={currentData}
-            exited={!isActive}
           />
         </div>
+      )}
+
+      {multiItems && isActive && !editingItem && (
+        <>
+          {existingItems?.map((item) => {
+            if (!item) return null;
+
+            if (isExperienceInfo(item.value)) {
+              const value = item.value;
+              return (
+                <div key={item.id} className="item-container">
+                  <IconButton
+                    type="delete"
+                    onClick={() => deleteItem(item.key, item.id)}
+                    className="delete-item"
+                  />
+                  <div className="item-title">
+                    <h2>{value.jobTitle}</h2>
+                    <p>at {value.employer}</p>
+                  </div>
+                  <div className="item-dates-place">
+                    <p>
+                      {value.startDate} - {value.finishDate}
+                    </p>
+                    <p className="place">{value.city}</p>
+                  </div>
+                  <div className="item-description">
+                    <p>{value.description}</p>
+                  </div>
+                </div>
+              );
+            }
+          })}
+
+          {nextSlot !== "-1" && (
+            <button
+              onClick={() => setEditingItem(true)}
+              className="submit-button"
+            >
+              <p>Add experience</p>
+            </button>
+          )}
+        </>
       )}
     </div>
   );
