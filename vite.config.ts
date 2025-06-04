330b362141be87d370221c3f03659499dd21d8a7
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-06-04 15:37:58 -0300

    fix: solve path alias issue in deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   plugins: [react()],
